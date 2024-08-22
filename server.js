302eb40bee329ff7e71cf84cfe588157270cf3f0
Sandra-0701 <sandrasindhu0701@gmail.com>
2024-08-22 20:00:19 +0530

    added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use environment port
 
 app.use(cors());
 app.use(express.json());
